de2645e95c875b09a43887b13c7e1024c118c859
Livy20240812 <LZLZLZ2020@OUTLOOK.COM>
2024-12-11 03:11:36 -0500

    20241211 Reviewing, next is List

---
+++ MyPlayground/CH4/CH4SelfPractice/CH4SelfPractice/ArrayOperation.cs
@@ -0,0 +1,80 @@
+﻿/*
+//1.初始化。
+//int[] nums = {1, 2};
+//int[] nums2 = [1, 3, 2, 4, 5];
+//foreach (var item in nums)
+//{
+//    Console.WriteLine(item);
+//}
+//foreach (var item in nums2)
+//{
+//    Console.WriteLine(item);
+//}
+
+//2.访问元素。
+//int[] nums = [1, 3, 4, 5, 2];
+//int RandomAccess(int[] nums)
+//{
+//    Random random = new Random();
+//    int randomIndex = random.Next(nums.Length);
+//    int randomNum = nums[randomIndex];
+//    return randomNum;
+//}
+
+//3.插入。
+//int[] nums = [1, 3, 4, 5, 2];
+//void Insert(int[] nums, int num, int index)
+//{
+//    for (global::System.Int32 i = nums.Length - 1; i > index; i--)
+//    {
+//        nums[i] = nums[i - 1];
+//    }
+//    nums[index] = num;
+//}
+
+//4.删除。
+//int[] nums = [1, 3, 4, 5, 2];
+//void Delete(int[] nums, int index)
+//{
+//    for (global::System.Int32 i = index; i < nums.Length - 1; i++)
+//    {
+//        nums[i] = nums[i + 1];
+//    }
+//}
+
+//5.遍历数组。
+//int[] nums = [1, 3, 4, 5, 2];
+//void Traverse(int[] nums)
+//{
+//    int count = 0;
+//    for (global::System.Int32 i = 0; i < nums.Length; i++)
+//    {
+//        count += nums[i];
+//    }
+//    foreach (var i in nums)
+//    {
+//        count += i;
+//    }
+//}
+
+//6.查找。
+//int Search(int[] nums, int n)
+//{
+//    for (global::System.Int32 i = 0; i < nums.Length; i++)
+//    {
+//        if(nums[i] == n) return i;
+//    }
+//    return -1;
+//}
+
+//7.扩容。
+//int[] Extend(int[] nums, int enlarge)
+//{
+//    int[] largeArray = new int[nums.Length + enlarge];
+//    for (global::System.Int32 i = 0; i < nums.Length; i++)
+//    {
+//        largeArray[i] = nums[i];
+//    }
+//    return largeArray;
+//}
+*/
