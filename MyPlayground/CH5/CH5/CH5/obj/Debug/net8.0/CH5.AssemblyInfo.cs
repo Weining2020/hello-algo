479d3cf353d6c82ed1e1049a4423ca30ca524c51
Li Lei <131417088+Weining2020@users.noreply.github.com>
2024-12-12 17:46:05 -0500

    20241212 Before Home

--- MyPlayground/CH5/CH5/CH5/obj/Debug/net8.0/CH5.AssemblyInfo.cs
+++ MyPlayground/CH5/CH5/CH5/obj/Debug/net8.0/CH5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CH5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90a790252c1ac0f1e0beaaad896ccf2a41936430")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5069922dee857c5e160b8a5ee287617809b91aba")]
 [assembly: System.Reflection.AssemblyProductAttribute("CH5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CH5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
