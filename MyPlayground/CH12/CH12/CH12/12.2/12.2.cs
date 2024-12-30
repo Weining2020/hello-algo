67c5caa02b8c0d380d414f9cb42dff0f5b7fd06d
Livy20240812 <LZLZLZ2020@OUTLOOK.COM>
2024-12-29 23:41:00 -0500

    20241229 Before Bed

---
+++ MyPlayground/CH12/CH12/CH12/12.2/12.2.cs
@@ -0,0 +1,30 @@
+﻿/*
+int DFS(int[] nums, int target, int i, int j)
+{
+    if (i > j)
+    {
+        return -1;
+    }
+
+
+    int m = i + (j - i) / 2;
+    if (nums[m] < target)
+    {
+        return DFS(nums, target, m + 1, j); //每次调用 DFS 方法时，它期望一个返回值（int 类型）。如果不写 return，递归调用的结果不会被传递回上一层，最终返回给主调用函数的结果将是默认值（通常是 0 或 -1，取决于你的初始化）。每一层递归调用都会依赖下一层递归的返回值。例如：如果 nums[m] < target，我们需要从 DFS(nums, target, m + 1, j) 中得到结果，并返回给上一层。没有 return，当前层递归的结果就会丢失。
+    }
+    else if (nums[m] > target)
+    {
+        return DFS(nums, target, i, m - 1);
+    }
+    else
+    {
+        return m;
+    }
+
+}
+int BinarySearchDFS(int[] nums, int target)
+{
+    int n = nums.Length - 1;
+    return DFS(nums, target, 0, n);
+}
+*/
---
+++ MyPlayground/CH12/CH12/CH12/Program.cs
@@ -0,0 +1,22 @@
+﻿void Move(List<int> src, List<int> tar)
+{
+    int pan = src[src.Count - 1];
+    src.RemoveAt(src.Count - 1);
+    tar.Add(pan);
+}
+void DFS(int i, List<int> src, List<int> buf, List<int> tar)
+{
+    if (i == 1)
+    {
+        Move(src, tar);
+        return;
+    }
+    DFS(i - 1, src, tar, buf);
+    Move(src, tar);
+    DFS(i - 1, buf, src, tar);
+}
+void SolveHanota(List<int> A, List<int> B, List<int> C)
+{
+    int n = A.Count;
+    DFS(n, A, B, C);
+}
