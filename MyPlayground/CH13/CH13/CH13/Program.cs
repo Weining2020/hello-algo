e02f1d9d7cdf6b66d35fceec6270f2f12fbc0e28
Livy20240812 <LZLZLZ2020@OUTLOOK.COM>
2025-01-14 23:13:52 -0500

    20250114 Before Bed

--- MyPlayground/CH13/CH13/CH13/Program.cs
+++ MyPlayground/CH13/CH13/CH13/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using hello_algo.utils;
+using System.Collections.Generic;
+
+List<TreeNode> path = new List<TreeNode>();
+List<List<TreeNode>> res = new List<List<TreeNode>>();
+void PreOrder(TreeNode? root)
+{
+    if (root == null)
+    {
+        return;
+    }
+
+    path.Add(root);
+    if (root.val == 7)
+    {
+        res.Add(new List<TreeNode>(path));
+    }
+
+    PreOrder()
+}
