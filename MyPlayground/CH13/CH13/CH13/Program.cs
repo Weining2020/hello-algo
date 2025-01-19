4f4298f5a756296406bd400bdb86fe65d8aeef46
Livy20240812 <LZLZLZ2020@OUTLOOK.COM>
2025-01-18 23:03:48 -0500

    20250118 Before Bed

--- MyPlayground/CH13/CH13/CH13/Program.cs
+++ MyPlayground/CH13/CH13/CH13/Program.cs
@@ -1,34 +1,51 @@
-﻿void Backtrack(List<int> state, int[] choices, bool[] selected, List<List<int>> res)
+﻿void Backtrack(List<List<string>> state, int row, int n, bool[] cols, bool[] diags1, bool[] diags2,List<List<List<string>>> res)
 {
-    if (state.Count == choices.Length)
+    if (row == n)
     {
-        res.Add(new List<int>(state));
+        List<List<string>> copyState = new List<List<string>>();
+        for (global::System.Int32 i = 0; i <= row; i++)
+        {
+            copyState.Add(new List<string>(state[i]));
+        }
+        res.Add(copyState);
         return;
     }
 
-    HashSet<int> duplicated = new HashSet<int>();
-
-    for (global::System.Int32 i = 0; i < choices.Length; i++)
+    for (global::System.Int32 col = 0; col < n; col++)
     {
-        int choice = choices[i];
-        if (!selected[i] && !duplicated.Contains(choice))
+        int diag1 = row - col + n - 1;
+        int diag2 = row + col;
+        if (!cols[col] && !diags1[diag1] && !diags2[diag2])
         {
-            duplicated.Add(choice);
-            selected[i] = true;
-            state.Add(choice);
+            cols[col] = true;
+            diags1[diag1] = true;
+            diags2[diag2] = true;
+            state[row][col] = "Q";
 
-            Backtrack(state, choices, selected, res);
+            Backtrack(state, row + 1, n, cols, diags1, diags2, res);
 
-            selected[i] = false;
-            state.RemoveAt(state.Count - 1);
+            cols[col] = false;
+            diags1[diag1] = false;
+            diags2[diag2] = false;
+            state[row][col] = "#";
         }
     }
+
+
 }
-List<List<int>> Permutations(int[] nums)
+List<List<List<string>>> NQueens(int n)
 {
-    List<List<int>> res = new List<List<int>>();
-
-    Backtrack(new List<int>(), nums, new bool[nums.Length], res);
-
+    List<List<List<string>>> res = new List<List<List<string>>>();
+    List<List<string>> state = new List<List<string>>();
+    for (global::System.Int32 i = 0; i < n; i++)
+    {
+        List<string> row = new List<string>();
+        for (global::System.Int32 j = 0; j < n; j++)
+        {
+            row.Add("#");
+        }
+        state.Add(row);
+    }
+    Backtrack(state, 0, n, new bool[n], new bool[2 * n - 1], new bool[2 * n - 1], res);
     return res;
 }
