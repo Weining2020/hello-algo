b67754afb0983b4727054ce1a0c04d9ac6907ffb
Livy20240812 <LZLZLZ2020@OUTLOOK.COM>
2025-01-25 23:23:34 -0500

    20250125 Before Bed

--- MyPlayground/CH14/CH14/CH14/Program.cs
+++ MyPlayground/CH14/CH14/CH14/Program.cs
@@ -1,23 +1,35 @@
-﻿
-int ClimbingStairsWithConstraintsDP(int n)
+﻿int EditDistanceDP(string s, string t)
 {
-    if (n == 1 || n == 2)
+    if (s == null && t == null)
     {
-        return 1;
+        return 0;
+    }
+    int n = s.Length;
+    int m = t.Length;
+    int[] dp = new int[m + 1];
+
+    for (global::System.Int32 j = 1; j <= m; j++)
+    {
+        dp[j] = j;
+    }
+    for (global::System.Int32 i = 1; i <= n; i++)
+    {
+        int leftup = dp[0];
+        dp[0] = i;
+        for (global::System.Int32 j = 1; j <= m; j++)
+        {
+            int temp = dp[j]; //dp[j]马上要被更新为本行的dp[j]值了，我们马上要丢失上一行的dp[j]值了，即对于内层for循环的下一列来说马上要丢失左上角值了。因此先在暂时保存。
+            if (s[i - 1] == t[j - 1])
+            {
+                dp[j] = dp[j - 1];
+            }
+            else
+            {
+                dp[j] = Math.Min(Math.Min(dp[j - 1], dp[j]), leftup) + 1;
+            }
+            leftup = temp;
+        }
     }
 
-    int[,] dp = new int[n + 1, 3];
-
-    dp[1, 1] = 1;
-    dp[1, 2] = 0;
-    dp[2, 1] = 0;
-    dp[2, 2] = 1;
-
-    for (global::System.Int32 i = 3; i <= n; i++)
-    {
-        dp[i, 1] = dp[i - 1, 2];
-        dp[i, 2] = dp[i - 2, 1] + dp[i - 2, 2];
-    }
-
-    return dp[n, 1] + dp[n, 2];
+    return dp[m];
 }
