6357936fbd31c287170a024b6429ddac1685c9a6
Livy20240812 <LZLZLZ2020@OUTLOOK.COM>
2025-02-19 23:13:04 -0500

    20250219 Before Bed

---
+++ MyPlayground/SelfPractice/CH4SelfPractice/CH4SelfPractice/20250219/4.2.1.cs
@@ -0,0 +1,70 @@
+﻿/*
+using CH4SelfPractice.utils.ListNode;
+
+//1.初始化LinkedList。
+ListNode node0 = new ListNode(0);
+ListNode node1 = new ListNode(1);
+ListNode node2 = new ListNode(2);
+ListNode node3 = new ListNode(3);
+ListNode node4 = new ListNode(4);
+
+node0.next = node1;
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+
+//2.随机访问。不可能。
+
+//2.添加节点。
+void Insert(ListNode n0, ListNode p)
+{
+    ListNode? n1 = n0.next;
+    p.next = n1;
+    n1.next = p;
+}
+
+//3.删除节点。
+void Remove(ListNode n0)
+{
+    if (n0.next == null)
+    {
+        return;
+    }
+    ListNode p = n0.next;
+    ListNode? n1 = p.next;
+    n0.next = n1;
+}
+
+
+
+//4.访问节点。
+ListNode? Access(ListNode? head, int index)
+{
+    for (global::System.Int32 i = 0; i < index; i++)
+    {
+        if (head == null)
+        {
+            return null;
+        }
+        head = head.next;
+    }
+    return head;
+}
+
+//5.查找节点。
+int Find(ListNode? head, int target)
+{
+    int index = 0;
+    while (head != null)
+    {
+        if (head.val == target)
+        {
+            return index;
+        }
+        head = head.next;
+        index++;
+    }
+    return -1;
+
+}
+*/
--- MyPlayground/SelfPractice/CH4SelfPractice/CH4SelfPractice/obj/Debug/net8.0/CH4SelfPractice.AssemblyInfo.cs
+++ MyPlayground/SelfPractice/CH4SelfPractice/CH4SelfPractice/obj/Debug/net8.0/CH4SelfPractice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CH4SelfPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90a790252c1ac0f1e0beaaad896ccf2a41936430")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bbc863856391459da7a59822f14dd1b69a847a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CH4SelfPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CH4SelfPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
