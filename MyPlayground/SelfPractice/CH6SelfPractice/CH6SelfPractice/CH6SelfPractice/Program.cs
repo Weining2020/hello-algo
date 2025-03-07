5902cf6dda62af78a481f46bf7303c2e5c52f4b9
Livy20240812 <LZLZLZ2020@OUTLOOK.COM>
2025-03-06 20:56:50 -0500

    20250306 Before Bed

---
+++ MyPlayground/SelfPractice/CH6SelfPractice/CH6SelfPractice/CH6SelfPractice/Program.cs
@@ -0,0 +1,29 @@
+﻿//初始化。
+Dictionary<int, string> map = new Dictionary<int, string>()
+{
+    {12345, "Livy" }
+};
+
+//添加。
+map.Add(12346, "Zhu");
+
+//查询。
+string name = map[12345];
+
+//删除。
+//在哈希表中删除键值对(key, Value)。
+map.Remove(12345);
+
+//遍历哈希表。
+foreach (var vk in map)
+{
+    Console.WriteLine(vk.Key + "->" + vk.Value);
+}
+foreach (var key in map.Keys)
+{
+    Console.WriteLine("Key is " + key);
+}
+foreach (var val in map.Values)
+{
+    Console.WriteLine("Value is " + val);
+}
