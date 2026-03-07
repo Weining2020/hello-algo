b7697d09efa332a60f30a40fbf7dbae4fead7046
Livy20240812 <LZLZLZ2020@OUTLOOK.COM>
2026-03-06 21:42:13 -0500

    20260306 Before Bed

---
+++ MyNewPlayground/MyNewPlayground/Class/CH4/4.2.3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyNewPlayground.Class.CH4
+{
+    internal class _423
+    {
+/*
+//双向链表节点类
+class ListNode(int x)
+{  // 构造函数
+    int val = x;    // 节点值
+    ListNode next;  // 指向后继节点的引用
+    ListNode prev;  // 指向前驱节点的引用
+}
+*/
+    }
+}
