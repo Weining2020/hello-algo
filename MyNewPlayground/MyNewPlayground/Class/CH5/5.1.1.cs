8e0052c681adcaae1007d9bcd66ddca2c99de40e
Livy20240812 <LZLZLZ2020@OUTLOOK.COM>
2026-03-18 21:51:58 -0400

    20260318 Before Bed

---
+++ MyNewPlayground/MyNewPlayground/Class/CH5/5.1.1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyNewPlayground.Class.CH5
+{
+    internal class _511
+    {
+        /*
+//初始化栈
+Stack<int> stack = [];
+
+//元素入栈
+stack.Push(1);
+stack.Push(3);
+stack.Push(2);
+stack.Push(5);
+stack.Push(4);
+
+//访问栈顶元素
+int peek = stack.Peek();
+
+//元素出栈
+int pop = stack.Pop();
+
+//获取栈的长度
+int size = stack.Count;
+
+bool isEmpty = stack.Count == 0;
+*/
+    }
+}
