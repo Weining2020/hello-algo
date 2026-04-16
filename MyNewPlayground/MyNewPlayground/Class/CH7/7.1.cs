8ed1e38379714baa54503416c33042c8d5b3ec87
Livy20240812 <LZLZLZ2020@OUTLOOK.COM>
2026-04-15 20:21:44 -0400

    20260415 Before Bed

---
+++ MyNewPlayground/MyNewPlayground/Class/CH7/7.1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyNewPlayground.Class.CH7
+{
+    internal class _71
+    {/*
+public class TreeNode
+{
+    public int? val;
+    public TreeNode? left;
+    public TreeNode? right;
+
+    public TreeNode(int? x)
+    {
+        val = x;
+    }
+}
+*/
+    }
+}
